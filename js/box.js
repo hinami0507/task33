5de06db99b4bbfc17fc6d414c3bc09a0c606738d
hinami0507 <346565386@qq.com>
2016-04-04 18:34:53 +0800

    1.3
    完善

--- js/box.js
+++ js/box.js
@@ -18,22 +18,24 @@ window.onload = function() {
     function createbox() {
         this.cunit = 50; //横坐标单位
         this.vunit = 50; //纵坐标单位
-        this.x = rnd(0, c - 1);
-        this.y = rnd(0, r - 1);
-        this.rotate = 0;
-        this.xDir = 0;
-        this.yDir = 0;
+        this.x = rnd(0, c - 1);  // 小方块当前x坐标
+        this.y = rnd(0, r - 1);  // 小方块当前y坐标
+        this.rotate = 0; //当前车头方向
+        this.xDir = 0;  //小方块x轴方向
+        this.yDir = 0;  //小方块y轴方向
         var oBlock = document.createElement('span');
         oBox.appendChild(oBlock);
         oBlock.className = 'block';
         oBlock.id = "car";
-        this.bound = function() { //边界设定
+        //边界设定
+        this.bound = function() { 
             if (this.y <= 0) this.y = 0;
             if (this.y >= (c - 1)) this.y = c - 1;
             if (this.x <= 0) this.x = 0;
             if (this.x >= (r - 1)) this.x = r - 1;
         }
-        this.go = function(x, y) { //运行
+        //运行
+        this.go = function(x, y) { 
             if (x) this.x += x;
             if (y) this.y += y;
             this.bound();
@@ -41,28 +43,30 @@ window.onload = function() {
             oBlock.style.left = this.cunit * this.x + 'px';
             oBlock.style.top = this.vunit * this.y + 'px';
         };
-        this.keyDrive = function(key) { //按键移动
+        //按键移动
+        this.keyDrive = function(key) { 
             switch (key) { //左上右下
-                case 37:
+                case 37: //左
                     this.rotate = -90;
                     this.x -= 1;
                     break;
-                case 38:
+                case 38: //上
                     this.rotate = 0;
                     this.y -= 1;
                     break;
-                case 39:
+                case 39: //右
                     this.rotate = 90;
                     this.x += 1;
                     break;
-                case 40:
+                case 40: //下
                     this.rotate = 180;
                     this.y += 1;
                     break;
             }
             this.go();
         };
-        this.nowDir = function() { //判断当前方向
+        //判断当前方向
+        this.nowDir = function() { 
             var dir = Math.abs((this.rotate / 90) % 4);
             var flag = ((this.rotate / 90) % 4) / dir; //判断左右，-1为左，1为右
             switch (dir) {
@@ -80,8 +84,8 @@ window.onload = function() {
                     break;
             }
         }
-        this.terminal = function() { //输入框输入移动
-            var sValue = oTxt.value;
+        //输入框输入移动
+        this.terminal = function(sValue) { 
             this.nowDir();
             switch (sValue) {
                 case 'GO':
@@ -101,17 +105,19 @@ window.onload = function() {
         }
     }
 
-
+    //实例化小方块
     var box1 = new createbox();
     box1.go();
-
+    //通过输入字符驱动
     oBtn.addEventListener("click", function() {
-        box1.terminal();
+        box1.terminal(oTxt.value);
     })
+    //通过键盘方向键驱动
     window.addEventListener("keydown", function(event) {
-        box1.keyDrive(event.keyCode);
-        console.log(event.keyCode)
+        if (enableKey.checked == true) //检查使能端是否打开
+            box1.keyDrive(event.keyCode);
     })
+    //通过点击tip快速输入
     var tip = document.getElementById("tip")
     tip.addEventListener("click", function(target) {
         if (target.target.nodeName == "H5") {
